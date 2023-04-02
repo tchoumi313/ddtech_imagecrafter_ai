cb6dd4fa7ba8cea5bf4c307d313c4da5e02c69aa
tchoumi313 <75470956+tchoumi313@users.noreply.github.com>
2023-04-02 09:32:26 +0100

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,13 +12,23 @@ const App = () => {
       justify-between items-center bg-[#0099ff]
       sm:px-8 px-4 border-b border-b-[#e6ebf4]"
       >
-        <Link to="/">
-          <img
-            src={logo}
-            alt="logo"
-            className="w-28 object-contain rounded-md "
-          />
-        </Link>
+        <div className="flex ">
+          <Link to="/">
+            <img
+              src={logo}
+              alt="logo"
+              className="w-28 object-contain rounded-md "
+            />
+          </Link>
+          <span className="text-center justify-center items-center mt-12">
+            <a
+              className="text-stone-800 text-sm hover:bg-white hover:text-blue-700 rounded-md"
+              href="https://donaldtportfolio.netlify.app"
+            >
+              &copy;2023 DonaldTCHOUMI
+            </a>
+          </span>
+        </div>
         <Link
           to="/create-post"
           className="font-inter font-medium bg-blue-700 text-white hover:bg-indigo-700 px-4 py-2 rounded-md"
